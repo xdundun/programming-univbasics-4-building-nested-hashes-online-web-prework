2b96f76e9bc0cbf94481aed2af4b4816395aae1d
Dardan Gjonbalaj <xdundun@gmail.com>
2019-10-26 22:58:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,7 @@ def monopoly_with_third_tier
 				:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200
 			},
 			:names => {
-				:key1 => {}, :key2 => {}, :key3 => {}, :key4 => {}
+				:reading_railroad => {}, :pennsylvania_railroad => {}, :key3 => {}, :key4 => {}
 			}
 
 	}
