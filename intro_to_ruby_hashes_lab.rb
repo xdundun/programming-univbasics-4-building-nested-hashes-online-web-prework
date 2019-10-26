08874c5763336a986e3af3e70e12d36e8ae14359
Dardan Gjonbalaj <xdundun@gmail.com>
2019-10-26 22:52:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
 		:railroads => {
 			:pieces => 4,
 			:rent_in_dollars => {
-				:key1 => {}, :key2 => {}, :key3 => {}, :key4 => {}
+				:names => {}, :key2 => {}, :key3 => {}, :key4 => {}
 			}
 	}
 }
